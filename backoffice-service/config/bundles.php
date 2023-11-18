57237c018b777e6aa7e1e00bb025fa898a26d76d
Tac Tacelosky <tacman@gmail.com>
2023-11-18 11:57:18 -0500

    update to Symfony 6.3, add debug

--- backoffice-service/config/bundles.php
+++ backoffice-service/config/bundles.php
@@ -6,4 +6,7 @@ return [
     Ecotone\SymfonyBundle\EcotoneSymfonyBundle::class => ['all' => true],
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
+    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
+    Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
+    Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true],
 ];
