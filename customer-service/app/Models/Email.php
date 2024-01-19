dead8e4bad742396c87bc48e5b600abe3f806157
JeiHO <chechomancr@gmail.com>
2024-01-19 00:43:24 -0600

    Error message description updated (#8)

--- customer-service/app/Models/Email.php
+++ customer-service/app/Models/Email.php
@@ -7,7 +7,7 @@ class Email
     public function __construct(public readonly string $address)
     {
         if (!filter_var($this->address, FILTER_VALIDATE_EMAIL)) {
-            throw new \InvalidArgumentException(sprintf("Email address %s is considered valid.", $this->address));
+            throw new \InvalidArgumentException(sprintf("Email address %s isn't considered valid.", $this->address));
         }
     }
 }
